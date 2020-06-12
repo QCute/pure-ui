e4d1b658ad31c1260ea9b99c4c42153e0ef22361
QCute <baldoor@qq.com>
2020-06-12 23:07:00 +0800

    Initial commit

---
+++ src/PureUI.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace QCute\PureUI;
+
+use Encore\Admin\Extension;
+
+class PureUI extends Extension
+{
+    public $name = 'pure-ui';
+
+    public $assets = __DIR__.'/../resources/assets';
+}
---
+++ src/PureUIServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace QCute\PureUI;
+
+use Encore\Admin\Admin;
+use Illuminate\Support\ServiceProvider;
+
+class PureUIServiceProvider extends ServiceProvider
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function boot(PureUI $extension)
+    {
+        if (!PureUI::boot())
+            return;
+
+        $vendor_path = 'vendor/laravel-admin-ext/pure-ui/';
+
+        if ($this->app->runningInConsole() && $assets = $extension->assets())
+            $this->publishes([$assets => public_path($vendor_path)], 'laravel-admin-pure-ui');
+
+        Admin::booting(function () use ($vendor_path) {
+            array_push(Admin::$baseCss, $vendor_path . 'PureAdminLTE/dist/css/pure.ui.min.css');
+            array_push(Admin::$baseJs, $vendor_path . 'PureAdminLTE/dist/css/pure.ui.min.js');
+        });
+    }
+}
